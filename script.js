2bdedbdc413138e54ee2a2d88cc18b5c5fad2a31
Rhonda <110504360+Rhondoid@users.noreply.github.com>
2022-10-10 18:29:46 -0600

    added Javascript

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //Let the game begin
+
+var questionOne="What is Brazil's top commodity?";
+var questionOneOptions="a,b,c,d"
+var questionTwo="What does Starbucks sell the most of?";
+var questionThree= "What drink do US citizens prefer?";
+var questionFour="What do many people prefer in their coffee?";
+var questions=["One", "Two", "Three", "Four"];
+var answerText="Coffee";
+var incorrectAnswer="Wrong!"
+
+var queStart="Start Quiz";
+var queResponse="Submit";
+
+const queStartBtn =document.getElementsByName("Start Quiz");
+const queResponse="Submit";
+const queQuestions=document.getElementsByName ("questions");
+const viewHighScores =document.getElementsByName("View High Scores");
+const resetBtn =document.getElementsByName("Time 0");
+const scoreBtn=document.getElementsByName("Enter Initials");
+
+//Hiding all but start of game
+const hide={
+  
+}
+const show={
+
+}
+
+for (let i=0; i< questions.length; i++) {
+
+}
+if (questions= "Coffee") {
+    questionTwo
+    
+} else {
+    "Wrong!", "timer -10 points"
+    
+}
+
+queStartBtn.addEventListener(click, "Start Quiz");
+"Submit".addEventListener(click, "Submit");
+viewHighScores.addEventListener(click,"View High Scores");
+scoreBtn.addEventListener(click, "Enter Initials");
+
